e30066e169cb86d43b1943161149ea67f434a546
Dev-Emmyy <emmanuelugochukwu2000@gmail.com>
2023-12-29 16:13:00 +0100

    push

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,30 @@
+'use client';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+
 
 export default function Home() {
+  const [apiData, setApiData] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('/api/rooms/getRooms');
+          const data = await response.json();
+          setApiData(data);
+          console.log(data)
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+      fetchData();
+    }, []);
+
   return (
-    <><div class="bg-[url('/img/banner-home.jpg')] bg-cover bg-center bg-no-repeat bg-center bg-local md:bg-fixed min-h-screen text-zinc-50 relative">
+    <>
+    <div class="bg-[url('/img/banner-home.jpg')] bg-cover bg-center bg-no-repeat bg-center bg-local md:bg-fixed min-h-screen text-zinc-50 relative">
 
       <div class="flex flex-col justify-between items-center">
         <div class="z-90 w-4/5 flex flex-col justify-center items-center">
@@ -19,17 +41,17 @@ export default function Home() {
 
         <div class="mt-28 mb-16 p-5 bg-slate-200 bg-opacity-50 w-9/12">
           <form action="/submit" method="post" class="flex flex-row justify-around items-center w-4/4">
-            <select id='adults_number' class="bg-transparent border-2 border-black-500 w-3/12 h-11 outline-none font-mono font-bold">
+            <select id='adults_number' class="bg-transparent border-2 border-black-500 w-3/12 h-11 outdivne-none font-mono font-bold">
               <option value="one">1 Adult</option>
               <option value="two">2 Adult</option>
               <option value="three">3 Adult</option>
             </select>
-            <select id='childrens_number' class="bg-transparent border-2 border-black-500 w-3/12 h-11 outline-none font-mono font-bold">
+            <select id='childrens_number' class="bg-transparent border-2 border-black-500 w-3/12 h-11 outdivne-none font-mono font-bold">
               <option value="one">1 Children</option>
               <option value="two">2 Childrens</option>
               <option value="three">3 Childrens</option>
             </select>
-            <select id='number_of_room' class="bg-transparent border-2 border-black-500 w-3/12 h-11 outline-none font-mono font-bold">
+            <select id='number_of_room' class="bg-transparent border-2 border-black-500 w-3/12 h-11 outdivne-none font-mono font-bold">
               <option value="one">1 Room</option>
               <option value="two">2 Rooms</option>
               <option value="three">3 Rooms</option>
@@ -50,7 +72,7 @@ export default function Home() {
             class="my-2.5"
             />
           <h2 class="my-2.5 text-5xl">Make Your Golden Memories</h2>
-          <p class="my-2.5 font-light">A 5-star hotel located near the airport and railway station; steps away from tourist sites like cave, hotel is ideal for leisure and business alike. Decorated with contemporary art featuring rooms and suites offering a view of the city skyline and the vast oasis; our huge ballroom and meeting spaces are ideal for business, and weddings.</p>
+          <p class="my-2.5 font-divght">A 5-star hotel located near the airport and railway station; steps away from tourist sites divke cave, hotel is ideal for leisure and business adivke. Decorated with contemporary art featuring rooms and suites offering a view of the city skydivne and the vast oasis; our huge ballroom and meeting spaces are ideal for business, and weddings.</p>
           <button class="my-2.5 text-[#2f4137] bg-[#EEE8AA] p-2 font-mono font-bold">Read More</button>
         </div>
         <div>
@@ -69,19 +91,19 @@ export default function Home() {
           <div class="flex justify-between items-center h-screen px-16 text-center">
             <div class="bg-[#2f4137] bg-opacity-50 py-5 px-10">
             <h6 class="text-[#EEE8AA] my-3 text-lg font-normal font-serif leading-5">Customer Support</h6>
-            <p class="text-[#EEE8AA] my-3 font-light leading-6 font-mono">24 Hours Support</p>
+            <p class="text-[#EEE8AA] my-3 font-divght leading-6 font-mono">24 Hours Support</p>
             </div>
             <div class="bg-[#2f4137] bg-opacity-50 py-5 px-10">
             <h6 class="text-[#EEE8AA] my-3 text-lg font-normal font-serif leading-5">Family Size Room</h6>
-            <p class="text-[#EEE8AA] my-3 font-light leading-6 font-mono">Available Room</p>
+            <p class="text-[#EEE8AA] my-3 font-divght leading-6 font-mono">Available Room</p>
             </div>
             <div class="bg-[#2f4137] bg-opacity-50 py-5 px-10">
             <h6 class="text-[#EEE8AA] my-3 text-lg font-normal font-serif leading-5">Conference Room</h6>
-            <p class="text-[#EEE8AA] my-3 font-light leading-6 font-mono">Conference Room Available</p>
+            <p class="text-[#EEE8AA] my-3 font-divght leading-6 font-mono">Conference Room Available</p>
             </div>
             <div class="bg-[#2f4137] bg-opacity-50 py-5 px-10">
             <h6 class="text-[#EEE8AA] my-3 text-lg font-normal font-serif leading-5">Free Wifi Corner</h6>
-            <p class="text-[#EEE8AA] my-3 font-light leading-6 font-mono">Free Wifi 24/7</p>
+            <p class="text-[#EEE8AA] my-3 font-divght leading-6 font-mono">Free Wifi 24/7</p>
             </div>
           </div>
         </div>
@@ -95,7 +117,7 @@ export default function Home() {
           </div>
           <div class="w-3/4 flex flex-col items-start text-left">
             <h2 class="text-[#2f4137] text-5xl font-serif font-normal tracking-widest mb-6">Our services</h2>
-            <p class="text-[#2f4137] font-light">We offer a range of services, including accommodation, dining, recreational facilities, event spaces, entertainment, concierge services, Wi-Fi, business services, transportation assistance, housekeeping, security, special packages, and responsive customer support. Emphasizing unique features and tailoring services to guest preferences is key.</p>
+            <p class="text-[#2f4137] font-divght">We offer a range of services, including accommodation, dining, recreational facidivties, event spaces, entertainment, concierge services, Wi-Fi, business services, transportation assistance, housekeeping, security, special packages, and responsive customer support. Emphasizing unique features and tailoring services to guest preferences is key.</p>
           </div>
         </div>
         <div class="flex w-full">
@@ -106,8 +128,8 @@ export default function Home() {
               width={350}
               height={450} 
               />
-              <h4 class="my-5 text-[#2f4137] text-4xl font-light">Spa &amp; Wellness</h4>
-              <p class="w-80 text-[#5a76660] font-mono">Our Spa & Wellness services offers guests a holistic experience focused on relaxation and rejuvenation. This includes comfortable accommodation, nutritious dining options, recreational facilities, and a comprehensive range of spa treatments, fitness programs, and holistic wellness practices.</p>
+              <h4 class="my-5 text-[#2f4137] text-4xl font-divght">Spa &amp; Wellness</h4>
+              <p class="w-80 text-[#5a76660] font-mono">Our Spa & Wellness services offers guests a hodivstic experience focused on relaxation and rejuvenation. This includes comfortable accommodation, nutritious dining options, recreational facidivties, and a comprehensive range of spa treatments, fitness programs, and hodivstic wellness practices.</p>
           </div>
           <div class="w-4/12">
             <Image
@@ -116,7 +138,7 @@ export default function Home() {
               width={350}
               height={450} 
               />
-              <h4 class="my-5 text-[#2f4137] text-4xl font-light">Events &amp; Meetings</h4>
+              <h4 class="my-5 text-[#2f4137] text-4xl font-divght">Events &amp; Meetings</h4>
               <p class="w-80 text-[#5a76660] font-mono">Our Events & Meetings provides versatile spaces and services to accommodate a range of gatherings. This includes conferences, meetings, weddings, and other events. </p>
           </div>
           <div class="w-4/12">
@@ -126,7 +148,7 @@ export default function Home() {
               width={350}
               height={450} 
               />
-              <h4 class="my-5 text-[#2f4137] text-4xl font-light">Restaurant &amp; Bar</h4>
+              <h4 class="my-5 text-[#2f4137] text-4xl font-divght">Restaurant &amp; Bar</h4>
               <p class="w-80 text-[#5a76660] font-mono">Our Restaurant & Bar offers a complete dining and social experience for guests. This includes on-site dining options with a diverse menu, ranging from local specialties to international cuisine. </p>
           </div>
         </div>
@@ -150,7 +172,7 @@ export default function Home() {
             width={100}
             height={100} 
             />
-            <p class="text-[#2f4137] font-light my-5">“We are proud to offer top range of comprehensive employment services such payroll and benefits administration management assistance with global ranch business range global business range employment.”</p>
+            <p class="text-[#2f4137] font-divght my-5">“We are proud to offer top range of comprehensive employment services such payroll and benefits administration management assistance with global ranch business range global business range employment.”</p>
           </div>
           <div class="flex flex-row justify-items-center items-start ">
             <div class="w-1/6 rounded-full">
@@ -169,6 +191,14 @@ export default function Home() {
           </div>
           </div>
         </div>
+      </div>
+
+      <div>
+      {apiData.map((item) => (
+          <div key={item.id}>
+            <p>{item.name}</p>
+          </div>
+        ))}
       </div>
       </>
   )
